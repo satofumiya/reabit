8be36253b1bb07bee7076e781fd8a76912375b6e
satofumiya <sato.workfree@gmail.com>
2023-03-26 17:01:21 +0900

    headerのアイコンなどのスタイルを調整

--- app/javascript/packs/header.js
+++ app/javascript/packs/header.js
@@ -3,13 +3,16 @@ document.addEventListener('DOMContentLoaded', () => {
 
   window.clickBtn1 = function () {
     const nav = document.getElementById("nav_for_Hamburger");
-    const btn = document.getElementById("toggle-btn")
+    const btn = document.getElementById("toggle-btn");
+    const logo = document.getElementById("logo");
     if (nav.style.display == "block") {
       nav.style.display = "none";
+      logo.style.display = "block";
       btn.className = "fas fa-bars"
     } else {
       nav.style.display = "block";
-      btn.className = "fa-solid fa-xmark"
+      logo.style.display = "none";
+      btn.className = "fa-solid fa-xmark col-6"
     }
   }
 });
