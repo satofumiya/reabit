53a54a4d599dd5c6f1ca766b08120227f4e95564
satofumiya <sato.workfree@gmail.com>
2023-04-12 20:03:22 +0900

    ユーザーページのデザインを修正しました

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,6 +6,8 @@ class PagesController < ApplicationController
   def show
     @user = User.find(params[:id])
     @books =  @user.books.where(finished: true)
+    @current_month_reads = @books.where(reading_at: Time.current.all_month)
+    @current_year_reads = @books.where(reading_at: Time.current.all_year)
     @posts = @user.posts
   end
 end
