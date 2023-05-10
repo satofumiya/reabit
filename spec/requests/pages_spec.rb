21c44b51b54a6015b6975759f307f7fdd053c328
satofumiya <sato.workfree@gmail.com>
2023-05-10 18:57:51 +0900

    トップページのシステムテストを作成しました

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -1,6 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe "Pages", type: :request do
+  before do
+    @user = FactoryBot.create(:user)
+    sign_in @user
+  end
+
   describe "GET /index" do
     it "returns http success" do
       get "/"
@@ -8,4 +13,10 @@ RSpec.describe "Pages", type: :request do
     end
   end
 
+  describe "GET /show" do
+    it "returns http success" do
+      get "/pages/#{@user.id}"
+      expect(response).to have_http_status(:success)
+    end
+  end
 end
